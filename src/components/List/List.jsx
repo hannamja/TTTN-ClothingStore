d9c05615676ec6bac7c1fba98f1d3d5ba7f1a013
nghiapham0207 <nghiapham02072001@gmail.com>
2023-10-18 23:37:23 +0700

    updated List component

--- src/components/List/List.jsx
+++ src/components/List/List.jsx
@@ -1,16 +1,13 @@
 import React from "react";
 import "./List.scss";
 import Card from "../Card/Card";
-import useFetch from "../../hooks/useFetch";
 
 const List = ({ data }) => {
-
-
   return (
     <div className="list">
-      {
-        data?.map((item) => <Card item={item} key={item.mamh} />)
-      }
+      {data?.length > 0
+        ? data?.map((item) => <Card item={item} key={item.mamh} />)
+        : "not found!"}
     </div>
   );
 };
