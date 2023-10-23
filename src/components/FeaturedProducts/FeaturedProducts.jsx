bac25ccfebfd23d1eddcc93a1225783da082610a
nghiapham0207 <nghiapham02072001@gmail.com>
2023-10-23 21:19:04 +0700

    added jquery cdn, updated FeatureProduct.js

--- src/components/FeaturedProducts/FeaturedProducts.jsx
+++ src/components/FeaturedProducts/FeaturedProducts.jsx
@@ -1,7 +1,18 @@
 import React from "react";
 import Card from "../Card/Card";
 import "./FeaturedProducts.scss";
+import OwlCarousel  from "react-owl-carousel"
+import 'owl.carousel/dist/assets/owl.carousel.css';
+import 'owl.carousel/dist/assets/owl.theme.default.css';
 // import useFetch from "../../hooks/useFetch";
+
+const owlOptions = {
+  items: 3,
+  loop: true,
+  margin: 10,
+  autoplay: true,
+  autoplayTimeout: 2000,
+}
 
 const FeaturedProducts = ({ type, dataSet }) => {
   // const { data, loading, error } = useFetch(
@@ -20,7 +31,9 @@ const FeaturedProducts = ({ type, dataSet }) => {
         </p>
       </div>
       <div className="bottom">
-        {dataSet?.map((item) => <Card item={item} key={item.mamh} />)}
+        <OwlCarousel className='owl-theme' {...owlOptions}>
+            {dataSet?.map((item) => <Card item={item} key={item.mamh} />)}
+        </OwlCarousel>
       </div>
     </div>
   );
