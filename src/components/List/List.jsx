d298ba0d7b59c6e832c090b5fc8f2ee68e44c8a7
kietdz <thelight3501@gmail.com>
2023-11-04 20:23:07 +0700

    search v0.1

--- src/components/List/List.jsx
+++ src/components/List/List.jsx
@@ -2,16 +2,14 @@ import React from "react";
 import "./List.scss";
 import Card from "../Card/Card";
 
-const List = ({ data, size }) => {
-
+const List = ({ data, size, color }) => {
+  if (size.length != 0) {
+    data = data.filter((item) => size.filter(s => item.ctMathangs.filter(ct => ct.sizeDTO.masize == s).length > 0).length > 0)
+  }
   return (
     <div className="list">
       {data?.length > 0
-        ? data?.map((item) =>
-          size.length != 0 ?
-            size.filter(s => item.ctMathangs.filter(ct => ct.sizeDTO.masize == s).length > 0).length > 0 ? <Card item={item} key={item.mamh} /> : <></>
-            : <Card item={item} key={item.mamh} />
-        )
+        ? data?.map((item) => <Card item={item} key={item.mamh} />)
         : "not found!"}
     </div>
   );
